2fca3dce83f99226490f66507ed424b14d189d01
Matthew Shore <mshorecode@gmail.com>
2023-07-29 16:39:32 -0500

    Added welcome card to the main.js

---
+++ data/students.js
@@ -0,0 +1,17 @@
+export const students = [
+  {
+    id: 1,
+    name: 'Harry Potter',
+    house: '',
+  },
+  {
+    id: 2,
+    name: 'Hermione Granger',
+    house: '',
+  },
+  {
+    id: 3,
+    name: 'Ron Weasley',
+    house: '',
+  }
+]
