d2613a01fdecd1dbf9c3b2f903de749c47b7a538
Matthew Shore <mshorecode@gmail.com>
2023-07-31 16:15:18 -0500

    Setup functionality for expelling students to the vold array and rendering the new card announcement on the deathEaters div

--- data/vold.js
+++ data/vold.js
@@ -1,17 +1,3 @@
 export const voldArmy = [
-  {
-    id: 1,
-    name: 'Harry Potter',
-    house: 'Gryffindor',
-  },
-  {
-    id: 2,
-    name: 'Hermione Granger',
-    house: 'Gryffindor',
-  },
-  {
-    id: 3,
-    name: 'Ron Weasley',
-    house: 'Gryffindor',
-  }
+  
 ]
